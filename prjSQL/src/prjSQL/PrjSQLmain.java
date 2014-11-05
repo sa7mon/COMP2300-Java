dee1a3095d011f246d42370607626f1bd27dcc0c
Dan Salmon <dan.sa7mon@gmail.com>
2014-11-05 14:45:18 -0600

    Add main class and utility class.

---
+++ prjSQL/src/prjSQL/PrjSQLmain.java
@@ -0,0 +1,15 @@
+package prjSQL;
+
+public class PrjSQLmain {
+
+	public static void main(String[] args) {
+		/* This is the main method of the program.  It will contain short statements that leverage
+		 * functions from SQLUtil.java.
+		 * 
+		 * By: Dan Salmon (https://github.com/sa7mon)
+		 * Created on: 11/5/14
+		 */
+
+	}
+
+}
---
+++ prjSQL/src/prjSQL/SQLutil.java
@@ -0,0 +1,9 @@
+package prjSQL;
+
+public class SQLutil {
+	/* SQLUtil - The class that will hold all of our useful functions for connecting to SQL
+	 * 
+	 * 
+	 */
+	
+}
