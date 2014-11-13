db39c0006da31d9e9238e2e6ba29c1f553c66c3c
Dan Salmon <dan.sa7mon@gmail.com>
2014-11-12 18:23:07 -0600

    Add imports to use mysql-connector-java-5.1.33-bin.jar

--- prjSQL/src/prjSQL/SQLutil.java
+++ prjSQL/src/prjSQL/SQLutil.java
@@ -1,4 +1,14 @@
+
 package prjSQL;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+
 
 public class SQLutil {
 	/* SQLUtil - The class that will hold all of our useful functions for connecting to SQL
