59133554c8192963834a1794c38c206cdb7ab2f6
Dan Salmon <dan.sa7mon@gmail.com>
2014-12-01 15:37:33 -0600

    Initial commit

---
+++ prjJavaFX/src/com/example/products/MainApp.java
@@ -0,0 +1,21 @@
+package com.example.products;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/* MainApp.java - The main Java source file.
+ * 
+ * Includes all of the calls to everything that compiles the program.
+ * 
+ */
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
