f3f89acd83b38acf83d70561fa3ef03c70a52079
Dan Salmon <dan.sa7mon@gmail.com>
2014-12-03 13:15:41 -0600

    Add empty Product class

---
+++ prjJavaFX/src/com/example/model/Product.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+/*
+ * Product.java - Contains all the functions and variables related to the Product class.
+ * 
+ * Will contain setters/getters/constructors.
+ * 
+ * Created: 12/3/14
+ */
+public class Product
+{
+
+}
