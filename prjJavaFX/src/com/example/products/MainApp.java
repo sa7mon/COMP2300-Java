3f7bd8ac96578c744b0b8d04c00c207e6dda9aa0
Dan Salmon <dan.sa7mon@gmail.com>
2014-12-03 14:35:06 -0600

    Fill in MainApp with functions and test data

--- prjJavaFX/src/com/example/products/MainApp.java
+++ prjJavaFX/src/com/example/products/MainApp.java
@@ -1,6 +1,18 @@
 package com.example.products;
 
+import java.io.IOException;
+
+import view.ShowEmployeeController;
+
+import com.example.model.Product;
+
+import employee.mainApp;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /* MainApp.java - The main Java source file.
@@ -9,10 +21,58 @@ import javafx.stage.Stage;
  * 
  */
 public class MainApp extends Application {
-
+   
+ //=========================== VARIABLES ============================//
+   private Stage primaryStage;
+   // Must match the pane type in the .fxml
+   private AnchorPane rootLayout;
+   
+   //Declare list that displays employees
+   private ObservableList<Product> products = FXCollections.observableArrayList();
+   
+   //============================ CONSTRUCTORS ========================//
+   
+   // Default constructor
+   public MainApp() {
+      products.add(new Product("Bonsai Tree", "40","$23.95","Get it for your desk!"));
+      products.add(new Product("Apple Tree", "23","$56.00","Yields in 5-15 years."));
+      products.add(new Product("Shrub","10","$12","Great for hiding things in."));
+      products.add(new Product("Rose Bush", "278","$78.50","Treat your lady."));
+   }
+   
+   // Getter for the list of products
+   public ObservableList<Product> getProductList() {
+      return products;
+   }
+   
+   public void initRootLayout() {
+      try {
+         // Load layout from .fxml file
+         FXMLLoader loader = new FXMLLoader();
+         loader.setLocation(MainApp.class.getResource("/view/ProductsOverview.fxml"));
+         rootLayout = (AnchorPane) loader.load();
+         
+         //Give controller access to the main app
+         ShowProductsController controller = loader.getController();
+         controller.setMainApp(this);
+         
+         // Show scene containing the root layout
+         Scene scene = new Scene(rootLayout);
+         primaryStage.setScene(scene);
+         primaryStage.show();
+         
+      } catch(IOException e) { e.printStackTrace();}
+   }
+   
+   public Stage getPrimaryStage () {
+      return primaryStage;
+   }
+   
 	@Override
 	public void start(Stage primaryStage) {
-		
+	   this.primaryStage = primaryStage;
+      this.primaryStage.setTitle("My Title");
+      initRootLayout();
 	}
 
 	public static void main(String[] args) {
